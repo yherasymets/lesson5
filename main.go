7d7d81f7218f4eebb30ff5e3481b2bcc9740ed50
yarikyarichek <yarikyarichek24@icloud.com>
2022-06-14 17:30:26 +0300

    home work to lesson5

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+const (
+	applePrice = 5.99
+	pearPrice  = 7
+	aplleCount = 9
+	pearCount  = 8
+)
+
+var myMoney int = 23
+
+func main() {
+	buyApple := aplleCount * applePrice
+	buyPear := pearCount * pearPrice
+	sum := buyApple + float64(buyPear)
+	fmt.Println("Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?", sum)
+
+	howMuchPear := myMoney / pearPrice
+	howMuchApple := float64(myMoney) / applePrice
+	fmt.Println("Скільки груш ми можемо купити?", howMuchPear)
+	fmt.Println("Скільки яблук ми можемо купити?", howMuchApple)
+
+	twoAndtwo := (2*applePrice + 2*pearPrice) < float64(myMoney)
+	fmt.Println("Чи ми можемо купити 2 груші та 2 яблука?", twoAndtwo)
+}
